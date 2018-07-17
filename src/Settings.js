21b57cd0352ab7ef3ef96b141a670ac26e935cd1
Julia Weaver <juliaw33@uw.edu>
2018-07-17 10:39:32 -0700

    finished settings page connection with firebase

--- src/Settings.js
+++ src/Settings.js
@@ -2,25 +2,48 @@ import React, { Component } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import {Checkbox, CheckboxGroup} from 'react-checkbox-group';
 
 export default class SettingsForm extends Component {
     constructor(props) {
         super(props)
         this.state = {
             newUsername: '',
-            newPassword: '',
-            weatherLocations: [],
+            primaryWeatherLocation: '',
+            secondaryWeatherLocation: '',
             newsSites: [],
-            googleCalCreds: []
+            gmail: '',
+            gmailPassword: ''
         }
     }
 
 
     handleSubmit() {
-        console.log(this.state.username);
-        console.log(this.state.password);
-        console.log(this.state.weatherLocations);
+        console.log(firebase.auth().currentUser)
+        console.log(this.state.newUsername);
+        console.log(this.state.primaryWeatherLocation);
+        console.log(this.state.secondaryWeatherLocation);
+        console.log(this.state.newsSites);
+        console.log(this.state.gmail)
+        console.log(this.state.gmailPassword)
 
+        let currentUser = firebase.auth().currentUser
+
+        if(this.state.newUsername){
+            currentUser.updateProfile({displayName: this.state.username})
+        }
+
+        
+        let userPrefRef = firebase.database().ref('UserPrefs').child(currentUser.uid)
+
+        userPrefRef.update({
+            username: this.state.newUsername,
+            primaryWeatherLocation: this.state.primaryWeatherLocation,
+            secondaryWeatherLocation: this.state.secondaryWeatherLocation,
+            newsSites: this.state.newsSites,
+            gmail: this.state.gmail,
+            gmailPassword: this.state.gmailPassword
+        })
     }
 
 
@@ -31,6 +54,12 @@ export default class SettingsForm extends Component {
         let changes = {};
         changes[field] = value;
         this.setState(changes);
+    }
+
+    newsSourcesChanged(newValues){
+
+        console.log(newValues)
+        this.setState({newsSites: newValues})
     }
 
     render() {
@@ -46,58 +75,53 @@ export default class SettingsForm extends Component {
                 </div>
 
                 <div className='form-group'>
-                    <label>Change Password:</label>
-                    <input type="password" className="form-control" name="newPassword" value={this.state.newPassword} onChange={(event) => { this.handleChange(event) }} />
-                </div>
-
-
-                <div className='form-group'>
                     <h2> Weather Settings </h2>
                     <label>Set Primary Weather Location:</label>
-                    <input className="form-control" name="firstWeatherLocation" value={this.state.weatherLocations[0]} onChange={(event) => { this.handleChange(event) }} />
+                    <input className="form-control" name="primaryWeatherLocation" value={this.state.primaryWeatherLocation} onChange={(event) => { this.handleChange(event) }} />
                 </div>
 
                 <div className='form-group'>
                     <label>Set Secondary Weather Location (optional):</label>
-                    <input className="form-control" name="secondWeatherLocation" value={this.state.weatherLocations[1]} onChange={(event) => { this.handleChange(event) }} />
+                    <input className="form-control" name="secondaryWeatherLocation" value={this.state.secondaryWeatherLocation} onChange={(event) => { this.handleChange(event) }} />
                 </div>
+
 
                 <div className='form-group'>
                     <h2>News Sources</h2>
                     <h3>Select Preferred News Sources (minimum 2, maximum 5):</h3>
-                    <ul>
-                        <li>
-                            <label>news source 1 </label>
-                            <input type='checkbox' className="form-control" name="news source 1" value={this.state.newsSites} onChange={(event) => { this.handleChange(event) }} />
-                        </li>
-                        <li>
-                            <label>news source 2 </label>
-                            <input type='checkbox' className="form-control" name="news source 2" value={this.state.newsSites} onChange={(event) => { this.handleChange(event) }} />
-                        </li>
-                        <li>
-                            <label>news source 3 </label>
-                            <input type='checkbox' className="form-control" name="news source 3" value={this.state.newsSites} onChange={(event) => { this.handleChange(event) }} />
-                        </li>
-                        <li>
-                            <label>news source 4 </label>
-                            <input type='checkbox' className="form-control" name="news source 4" value={this.state.newsSites} onChange={(event) => { this.handleChange(event) }} />
-                        </li>
-                        <li>
-                            <label>news source 5 </label>
-                            <input type='checkbox' className="form-control" name="news source 5" value={this.state.newsSites} onChange={(event) => { this.handleChange(event) }} />
-                        </li>
-                        <li>
-                            <label>news source 6 </label>
-                            <input type='checkbox' className="form-control" name="news source 6" value={this.state.newsSites} onChange={(event) => { this.handleChange(event) }} />
-                        </li>
-                        <li>
-                            <label>news source 7 </label>
-                            <input type='checkbox' className="form-control" name="news source 7" value={this.state.newsSites} onChange={(event) => { this.handleChange(event) }} />
-                        </li>
-                    </ul>
+
+                    <CheckboxGroup name="newSources" onChange={(event) => { this.newsSourcesChanged(event) }}>
+
+                                <Checkbox value="abc"/>
+                                <label>ABC </label>
+
+                                <Checkbox value="fox"/>
+                                <label>FOX </label>
+
+                                <Checkbox value="bbc"/>
+                                <label>BBC </label>
+
+                                <Checkbox value="cnn"/>
+                                <label>CNN </label>
+
+                                <Checkbox value="NY_Times"/>
+                                <label>NY Times </label>
+
+                                <Checkbox value="Washington_Post"/>
+                                <label>Washington Post </label>
+
+
+                    </CheckboxGroup>
                 </div>
 
+                <div className="form-group">
+                    <h2> Google Calender Log-in </h2>
+                    <label> Gmail: </label>
+                    <input className="form-control" name='gmail' value={this.state.gmail} onChange={(event) => { this.handleChange(event) }} />
+                    <label> Gmail Password: </label>
+                    <input className="form-control" name='gmailPassword' value={this.state.gmailPassword} onChange={(event) => { this.handleChange(event) }} />
 
+                </div>
 
                 <div className='form-group mb-5'>
                     <button className="btn btn-primary mr-2" onClick={() => this.handleSubmit()}> Submit Changes </button>
@@ -108,16 +132,4 @@ export default class SettingsForm extends Component {
     }
 }
 
-//Values to set:
 
-//Prefered news sites: checkboxes, can have at least 3? max? only 3?
-
-//Username input box
-
-
-//Weather locations
-//up to 3? more?
-
-//Google Calendar credentials??
-
-//SUbmit button!!!
