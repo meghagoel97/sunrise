b8359cdbd01ae05c8f44f815932e02d70d0d37ae
meghagoel97 <meghag@uw.edu>
2018-07-17 11:22:23 -0700

    please work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,105 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'whatwg-fetch';
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 
-class App extends Component {
+
+export class App extends Component {
   render() {
     return (
       <div>
+        <News />
       </div>
     );
   }
 }
 
 export default App;
+
+export class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      error: ''
+    };
+  }
+  // DONT FORGET ATTRIBUTION LINK FOR NEWS API
+  download() {
+    // Politico API
+    var url = 'https://newsapi.org/v2/top-headlines?sources=politico&apiKey=f1820d94c3744a42958a0465be7d21e5';
+    var req = new Request(url);
+    fetch(req)
+      .then(function (response) {
+        console.log(response.json());
+      })
+
+    // sort by popularity
+    var url2 = 'https://newsapi.org/v2/top-headlines?sources=the-new-york-times&from=2018-07-17&to=2018-07-17&sortBy=popularity&apiKey=f1820d94c3744a42958a0465be7d21e5';
+    var req2 = new Request(url2);
+    fetch(req2)
+      .then((response) => {
+        // console.log(response.json());
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data.articles);
+        this.setState({
+          articles: data.articles
+        });
+      }).catch((error) => {
+        this.setState({
+          error: error.message
+        });
+      });
+
+  }
+
+  componentDidMount() {
+    this.download();
+  }
+
+  render() {
+    let data = this.state.articles;
+    console.log(data);
+    let articles = data.map((article) => {
+      let nameURL = article.author;
+      let nameArray = nameURL.split('by/');
+      let name = '';
+
+      if (nameArray[1].includes('www')) {
+        name = nameArray[2];
+      } else {
+        name = nameArray[1];
+      }
+      return (<Card body>
+        <CardTitle> <a href={article.url}> {article.title} </a> </CardTitle>
+        <CardText> <small> {name} </small> </CardText>
+        <CardText> {article.description} </CardText>
+      </Card>);
+    })
+    return (<Row>
+      <Col sm='3'>
+        {articles[0]}
+      </Col>
+      <Col sm='3'>
+        {articles[1]}
+      </Col>
+      <Col sm='2'>
+        <Row> {articles[2]} </Row>
+        <Row> {articles[3]} </Row>
+      </Col>
+      <Col sm='2'>
+        <Row> {articles[4]} </Row>
+        <Row> {articles[5]} </Row>
+      </Col>
+      <Col sm='1'>
+        <Row> {articles[6]} </Row>
+        <Row> {articles[7]} </Row>
+      </Col>
+      <Col sm='1'>
+        <Row> {articles[8]} </Row>
+        <Row> {articles[9]} </Row>
+      </Col>
+    </Row>);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,19 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import 'bootstrap/dist/css/bootstrap.css';
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/database';
 
 
-var config = {
-    apiKey: "AIzaSyAYQnow1D-GsGFX3hsHJ1fKrEPNaQB-D5o",
-    authDomain: "info343-sunrise.firebaseapp.com",
-    databaseURL: "https://info343-sunrise.firebaseio.com",
-    projectId: "info343-sunrise",
-    storageBucket: "info343-sunrise.appspot.com",
-    messagingSenderId: "560688272292"
-  };
-  firebase.initializeApp(config);
+
+// var config = {
+//     apiKey: "AIzaSyAYQnow1D-GsGFX3hsHJ1fKrEPNaQB-D5o",
+//     authDomain: "info343-sunrise.firebaseapp.com",
+//     databaseURL: "https://info343-sunrise.firebaseio.com",
+//     projectId: "info343-sunrise",
+//     storageBucket: "info343-sunrise.appspot.com",
+//     messagingSenderId: "560688272292"
+//   };
+//   firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
