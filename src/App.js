49f266ebb0623d0981c6d66f368409a80d147026
Sagar Surana <Sagar@D-10-19-20-168.dhcp4.washington.edu>
2018-07-17 12:44:55 -0700

    basic files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Weather from './Weather.js'
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Weather />
     );
   }
 }
 
 export default App;
+
---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Weather.css';
+import './App.css';
+
+class Weather extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Weather;
