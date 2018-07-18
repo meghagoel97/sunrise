2374f465237324e8c24d8226df3cfe4d9966038d
Sagar Surana <Sagar@D-10-19-20-168.dhcp4.washington.edu>
2018-07-18 12:46:14 -0700

    everything finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import SignUpForm from './SignUp'; 
+import SignUpForm from './SignUp';
 import SettingsForm from './Settings'
 import SignInForm from './SignIn'
-import {BrowserRouter, Route, Switch, Link, NavLink, Redirect} from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link, NavLink, Redirect } from 'react-router-dom'
 import Weather from './Weather.js'
 import News from './News'
 
@@ -11,49 +11,49 @@ export default class App extends Component {
 
     return (
       <div>
-      <BrowserRouter>
-        <div className="container">
-          <h1> Sunrise </h1>
-          <nav>
-            <ul className='nav'>
-              <li>
-                <NavLink exact to='/' className="nav-link"> Home </NavLink>
-              </li>
-              <li>
-                <NavLink to='/Settings' className="nav-link"> Settings </NavLink>
-              </li>
-              <li>
-                <NavLink to='/SignIn' className="nav-link"> Sign-In </NavLink>
-              </li>
-              <li>
-                <NavLink to='/Weather' className="nav-link"> Weather </NavLink>
-              </li>
-              <li>
-                <NavLink to='/News' className="nav-link"> News </NavLink>
-              </li>
-            </ul>
-          </nav>
+        <BrowserRouter>
+          <div className="container">
+            <h1> Sunrise </h1>
+            <nav>
+              <ul className='nav'>
+                <li>
+                  <NavLink exact to='/' className="nav-link"> Home </NavLink>
+                </li>
+                <li>
+                  <NavLink to='/Settings' className="nav-link"> Settings </NavLink>
+                </li>
+                <li>
+                  <NavLink to='/SignIn' className="nav-link"> Sign-In </NavLink>
+                </li>
+                <li>
+                  <NavLink to='/Weather' className="nav-link"> Weather </NavLink>
+                </li>
+                <li>
+                  <NavLink to='/News' className="nav-link"> News </NavLink>
+                </li>
+              </ul>
+            </nav>
 
-          <Switch>
+            <Switch>
               <Route exact path="/" component={SignUpForm} />
-              <Route path="/Settings"  render= {(routerProps) => {
+              <Route path="/Settings" render={(routerProps) => {
                 return <SettingsForm {...routerProps} currentUser={this.props.currentUser} />
               }
-              }/>
-              <Route path="/SignIn" component={SignInForm}/>
+              } />
+              <Route path="/SignIn" component={SignInForm} />
 
-             {/* <Route path="/Weather"  render= {(routerProps) => {
+              <Route path="/Weather" render={(routerProps) => {
                 return <Weather {...routerProps} currentUser={this.props.currentUser} primaryWeather={this.props.userInfo.primaryWeatherLocation} secondaryWeather={this.props.userInfo.secondaryWeatherLocation} thirdWeather={this.props.userInfo.thirdWeatherLocation} />
               }
-              }/> */}
-             <Route path="/News"  render= {(routerProps) => {
+              } />
+              <Route path="/News" render={(routerProps) => {
                 return <News {...routerProps} currentUser={this.props.currentUser} newsSites={this.props.userInfo.newsSites} />
               }
-              }/>
-          </Switch>
+              } />
+            </Switch>
 
-        </div>
-      </BrowserRouter>
+          </div>
+        </BrowserRouter>
       </div>)
   }
 }
